fe6999cafd2bd928fa93d7bf03289981902c3785
tanhaibin <haibin.tan@fuwo.com>
2019-02-18 18:01:46 +0800

    -save

--- src/app/MultiPoint/MultiPoint.component.ts
+++ src/app/MultiPoint/MultiPoint.component.ts
@@ -11,10 +11,16 @@ export class MultiPointComponent implements OnInit {
   VSHADER_SOURCE: string =
     'attribute vec4 a_Position;\n' +
     'attribute float a_PointSize;\n' +
+    'uniform vec4 u_Translation;\n'+
+    'uniform float u_CosB,u_SinB;\n'+
     'void main(){\n' +
-    '  gl_Position=a_Position;\n' +
+    '  gl_Position.x=a_Position.x*u_CosB-a_Position.y*u_SinB;\n' +
+    '  gl_Position.y=a_Position.x*u_SinB+a_Position.y*u_CosB;\n' +
+    '  gl_Position.z=a_Position.z;\n' +
+    '  gl_Position.w=1.0;\n' +
     '  gl_PointSize=a_PointSize;\n' +
     '}\n';
+
   FSHADER_SOURCE: string =
     'precision mediump float;\n' +
     'uniform vec4 u_FragColor;\n' +
@@ -24,7 +30,7 @@ export class MultiPointComponent implements OnInit {
 
   initVertexBuffers(gl: WebGLRenderingContext): number {
     const vertices = new Float32Array([
-      0.0, 0.5, -0.5, -0.5, 0.5, -0.5
+      0.0, 0.5, -0.5,-0.5, 0.5, -0.5
     ]);
     const n = vertices.length / 2;
     const vertexBuffer = gl.createBuffer();
@@ -40,22 +46,32 @@ export class MultiPointComponent implements OnInit {
     return n;
 
   }
-
+  public ANGLE=-30.0;
   ngOnInit() {
     this.canvas = document.getElementById('webgl') as HTMLCanvasElement;
     const gl = getWebGLContext(this.canvas);
     initShaders(gl, this.VSHADER_SOURCE, this.FSHADER_SOURCE);
+    var radian=Math.PI*this.ANGLE/180.0;
+    var cosB=Math.cos(radian)
+    var sinB=Math.sin(radian)
     const n = this.initVertexBuffers(gl);
     const a_Position = gl.getAttribLocation(gl.program, 'a_Position');
     const a_PointSize = gl.getAttribLocation(gl.program, 'a_PointSize');
     const u_FragColor = gl.getUniformLocation(gl.program, 'u_FragColor');
+    const u_Translation = gl.getUniformLocation(gl.program, 'u_Translation');
+
+    const u_CosB = gl.getUniformLocation(gl.program, 'u_CosB');
+    gl.uniform1f(u_CosB, cosB);
+    const u_SinB = gl.getUniformLocation(gl.program, 'u_SinB');
+    gl.uniform1f(u_SinB, sinB);
+
     const g_points = [];
     const g_colors = [];
     gl.vertexAttrib1f(a_PointSize, 10.0);
     gl.uniform4f(u_FragColor, 1.0, 0.0, 0.0, 1.0);
-    gl.clearColor(0.0, 0.0, 0.0, 1);
+    gl.uniform4f(u_Translation, 0.5, -0.5, 0.0, 0.0);
+    gl.clearColor(0.0, 0.0, 0.0, 1.0);
     gl.clear(gl.COLOR_BUFFER_BIT);
-    gl.drawArrays(gl.POINTS, 0, n);
-
+    gl.drawArrays(gl.TRIANGLES, 0, n);
   }
 }
